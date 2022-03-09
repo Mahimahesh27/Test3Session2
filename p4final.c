cacd3ebc32a1b84444828a371cbc827d5c4ad8f4
Mahesh <94962227+Mahimahesh27@users.noreply.github.com>
2022-03-09 09:01:27 +0000

    done

--- p4final.c
+++ p4final.c
@@ -1,36 +1,57 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void input_n_and_r(int *n, int *r)
+int input_degree()
 {
-  printf("enter n and r\n");
-  scanf("%d%d",n,r);
+  int n;
+  printf("Enter degree of the polynomial\n");
+  scanf("%d",&n);
+  return n;
 }
 
-int factorial(int n)
- {
-	int factorial = 1;
-	for (int i = 2; i <= n; i++)
-		factorial = factorial * i;
-	return factorial;
+float input_x()
+{
+  float x;
+  printf("Enter the value of x\n");
+  scanf("%f",&x);
+  return x;
 }
 
-int ncr(int n, int r)
+void input_coefficients(int n, float a[n])
 {
-	return factorial(n) / (factorial(r) * factorial(n - r));
+ printf("Enter coefficient's of the polynomial x \n");
+  int i;
+  for(i=n;i>=0;i--)
+  {
+    printf("Enter Coefficient of [ X^%d ] \n",i);
+    scanf("%f",&a[i]);
+  }
 }
 
-void output(int n, int r, int result)
+float evaluate_polynomial(int n, float a[n], float x)
 {
-  printf("the ncr of %d and %d is %d",n,r,result);
+  float sum=0;
+  int i;
+  for(i=n;i>0;i--)
+   {
+     sum=(sum+a[i])*x;
+   }
+  sum=sum+a[0];
+  return sum;
+}
+
+void out_put(int n, float a[n], float x, float result)
+{
+  printf("Value of the polynomial is = [ %f ]\n",result);
 }
 
 int main()
 {
-	int n ,r,f,result;
-  input_n_and_r(&n,&r);
-  f=factorial(n);
-  result=(n,r);
-	result=ncr(n, r);
-  output(n,r,result);
-	return 0;
+  float a[100],result,x;
+  int n;
+  n=input_degree();
+  x=input_x();
+  input_coefficients(n,a);
+  result=evaluate_polynomial(n,a,x);
+  out_put(n,a,x,result);
+  return 0;
 }
